81f700ade5b9cb4f0690f17072b146df5cc31975
Julio Jimenez <zaor.julio@gmail.com>
2024-04-25 11:18:49 -0500

    validación de filas validas o archivo vacio

--- app/Dominio/Datos/Servicios/ServicioImportarVehiculos.ts
+++ app/Dominio/Datos/Servicios/ServicioImportarVehiculos.ts
@@ -118,11 +118,11 @@ export class ServicioImportarVehiculos {
   }
 
   async validarVehiculos(hoja: Excel.Worksheet, poliza: number): Promise<ErrorFormatoImportarExcel[]> {
-   /*  const marcas = await TblMarcas.all()
-    const nombresMarcas = marcas.map(marca => marca.nombre) */
     let errores: ErrorFormatoImportarExcel[] = []
+    let seEncontroFilaValida = false; 
     hoja.eachRow(fila => {
       if (fila.number > 1) {
+        seEncontroFilaValida = true;
         const placa = fila.getCell('A').value?.valueOf()
         const pasajeros = fila.getCell('B').value?.valueOf()
         //Validar existencia
@@ -169,6 +169,15 @@ export class ServicioImportarVehiculos {
 
       }
     })
+
+    if (!seEncontroFilaValida) {
+      errores.push({
+        columna: '',
+        fila: '',
+        error: 'El archivo está vacío o no contiene filas válidas.',
+        valor: null
+      });
+    }
     return errores
   }
 
@@ -190,113 +199,6 @@ export class ServicioImportarVehiculos {
     })
   }
 
- /*
-  importModalidades = async (
-    colComment: Excel.Column,
-    idVigilado: string,
-    hoja: Excel.Worksheet,
-    vigencia: number,
-    mes: number
-  ): Promise<Resultado<RespuestaImportacionExcel>> => {
-    const errores = await this.validarModalidades(hoja)
-    if(errores.length > 0){
-      const archivoB64 = await this.generarCsvErrores(errores)
-      return new Resultado({
-        exitoso: false,
-        estado: 422,
-        datos: {errores, archivo: archivoB64},
-        mensaje: 'Hay errores de validación.'
-      })
-    }
-    await TblVehiculosModalidades.query().where({ 'vep_vigilado': idVigilado, 'vep_vigencia': vigencia, 'vep_mes': mes }).delete();
-    colComment.eachCell(async (cell, rowNumber) => {
-      if (rowNumber >= 2) {
-        const nit = hoja.getCell('A' + rowNumber).value?.valueOf()
-        const modalidad = hoja.getCell('B' + rowNumber).value?.valueOf()
-        const placa = hoja.getCell('C' + rowNumber).value?.valueOf()
-        if (placa !== '' && nit !== '' && modalidad !== '') {
-
-          //custom field name in database to variable
-          const inputPlaca: VehiculoModalidad = {
-            nit: nit!.toString(),
-            placa: placa!.toString().toLocaleLowerCase(),
-            modalidadId: modalidad!.toString(),
-            vigilado: idVigilado,
-            vigencia,
-            mes
-          }
-          try {
-            const vehiculo = new TblVehiculosModalidades()
-            vehiculo.estableceVehiculoConId(inputPlaca)
-            await vehiculo.save()
-            console.log(vehiculo.id);
-          } catch (error) {
-            console.log(`la placa ${placa} ya existe`);
-          }
-        }
-      }
-    })
-    return new Resultado({
-      exitoso: true,
-      estado: 200
-    })
-  }
-
-  
-
-  async validarModalidades(hoja: Excel.Worksheet): Promise<ErrorFormatoImportarExcel[]> {
-    const modalidades = await TblModalidades.query()
-    const idModalidades = modalidades.map(modalidad => modalidad.id)
-    let errores: ErrorFormatoImportarExcel[] = []
-    hoja.eachRow(fila => {
-      if (fila.number > 1) {
-        const nit = fila.getCell('A').value?.valueOf()
-        const idModalidad = fila.getCell('B').value?.valueOf()
-        const numeroPlaca = fila.getCell('C').value?.valueOf()
-        //Validar exitencia
-        if (!nit || nit === '') {
-          errores.push({
-            columna: 'A',
-            fila: fila.number.toString(),
-            error: 'El valor no puede ser vacío.',
-            valor: nit
-          })
-        }
-
-        if (!idModalidad || idModalidad === '') {
-          errores.push({
-            columna: 'B',
-            fila: fila.number.toString(),
-            error: 'El valor no puede ser vacío.',
-            valor: idModalidad
-          })
-        } else {
-          if (typeof idModalidad === 'string' || typeof idModalidad === 'number') {
-            const existeModalidad = idModalidades.includes(Number(idModalidad))
-            if (!existeModalidad) {
-              errores.push({
-                columna: 'B',
-                fila: fila.number.toString(),
-                error: `No existe la modalidad con id: ${idModalidad}`,
-                valor: idModalidad
-              })
-            }
-          }
-        }
-
-        if (!numeroPlaca || numeroPlaca === '') {
-          errores.push({
-            columna: 'C',
-            fila: fila.number.toString(),
-            error: 'El valor no puede ser vacío.',
-            valor: numeroPlaca
-          })
-        } else { }
-      }
-    })
-    return errores
-  }
-
- */
+ 
 
 }
